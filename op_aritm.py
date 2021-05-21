c59acff77cc1edf3ebececb7eb7011581374f536
Instructor <jbedoyap.fomracion@gmail.com>
2021-05-20 21:18:23 -0500

    Version 1 con variable R

---
+++ op_aritm.py
@@ -0,0 +1,28 @@
+#ejercicios para clase 18/05/2021 operadores
+
+r=5
+'''r1=r
+
+print('Valor de r ', r1)
+r1= 'ya no es 5 sino una cadena de texto'
+print('Valor de r ', r1)
+
+#Suma el valor a la variable
+suma=10
+suma +=5
+print('\nEl valor de la vriable suma es: ', suma)
+
+
+
+# Multiplica el valor a la variable
+producto = 0
+# operacion sensible
+producto = -6*3%2
+print('\nEl valor de la variable suma es: ', producto)
+
+'''
+r = 0
+# determine si dos numeros son iguales.
+n = int(input(print("Ingrese numero n de la sucesion: \n")))
+print('El valor de la sucesión cuando n vale:',n, ' es:', 2**(n-1))
+

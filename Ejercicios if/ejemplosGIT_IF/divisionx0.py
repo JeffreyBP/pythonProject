fcd57a1dc4feb0ee531550a8e0780e990714fa54
Instructor <jbedoyap.fomracion@gmail.com>
2021-05-20 21:00:46 -0500

    Version comparacion sin IF

---
+++ Ejercicios if/ejemplosGIT_IF/divisionx0.py
@@ -0,0 +1,8 @@
+#Realice un programa que permita SABER si dos numeros son iguales
+#Realizado por Jeffrey vesion 1.0
+# Variables
+num_a =54
+num_b =54
+
+num_c = (num_a == num_b)
+print('La igualdad es ', num_c)
